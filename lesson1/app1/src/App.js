ea35f59ba15c29e42c6bafcbc96c9078c15863da
Danil <danil@davletov.net>
2023-10-12 18:26:04 +0300

    upd Button

--- lesson1/app1/src/App.js
+++ lesson1/app1/src/App.js
@@ -1,14 +1,18 @@
 import logo from "./logo.svg";
 import "./App.scss";
 import Card from "./components/Card";
-import Button from "./components/Button";
+import Button, {Button2} from "./components/Button";
+
+
 
 function App() {
   return (
     <div className="container">
 
       <h1>Cats</h1>
-      <Button>Кнопка</Button>
+      <Button hint="Нажми меня" customClassName="btn-primary">
+        <i>sdfhgsd</i>
+      </Button>
       <Card />
       <Card />
       <Card />
--- lesson1/app1/src/components/Button.js
+++ lesson1/app1/src/components/Button.js
@@ -1,5 +1,14 @@
 export default (props) => {
-    return (
-        <a href="" className="button">{props.children}</a>
-    )
-}
+    console.log(props);
+  return (
+    <a href="" className={props.customClassName} title={props.hint}>
+      {props.children}
+    </a>
+  );
+};
+
+const Button2 = function () {
+  return <a>Я вторая кнопка</a>;
+};
+
+export { Button2 };
