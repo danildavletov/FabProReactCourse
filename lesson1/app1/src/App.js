9e85b0191a39bc290f5c6a51fc041595144fec2a
Danil <danil@davletov.net>
2023-10-12 19:58:46 +0300

    добавили фильтр

--- lesson1/app1/src/App.js
+++ lesson1/app1/src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.scss";
 import Card from "./components/Card";
 import Button, { Button2 } from "./components/Button";
+import { TextField } from "@mui/material";
 
 
 let catsInitial = [
@@ -11,26 +12,36 @@ let catsInitial = [
     consumption: 2,
   },
   {name: "Рыжик"},
+  {name: "Пыжик"},
   {name: "Мурка",consumption: 1},
 ];
 
 function App() {
   const [cats, setCats] = useState(catsInitial);
+  const [filteredCats, setFilteredCats] = useState(catsInitial);
 
-  const handleClick = (e) => {
-    e.preventDefault();
+  const handleClick = (element) => {
+    element.preventDefault();
     setCats([
       {name: "Новый кот без имени"},
       ...cats,
     ]);
   };
+  const handleChange = (e) => {
+    let value = e.target.value;
+    let filter = cats.filter( (cat)=> cat.name.includes(value));
+    setFilteredCats(filter);
+  }
   return (
     <div className="container">
       <h1>Cats</h1>
 
-      {cats.map((cat) => (
+      <TextField onChange={handleChange} id="outlined-basic" label="Фильтр котов" variant="outlined" />
+
+      {filteredCats.map((cat) => (
         <Card name={cat.name} foodVolume={cat.consumption} />
       ))}
+
       <button onClick={handleClick}>Добавить котов</button>
     </div>
   );
