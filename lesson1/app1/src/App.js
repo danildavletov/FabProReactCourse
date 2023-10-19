e4a59795c093f3fc78475c9132edc108d6decbd1
Danil <danil@davletov.net>
2023-10-19 15:09:44 +0300

    изменил кнопку

--- lesson1/app1/src/App.js
+++ lesson1/app1/src/App.js
@@ -1,19 +1,24 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.scss";
 import Card from "./components/Card";
 import Button, { Button2 } from "./components/Button";
 import { TextField } from "@mui/material";
 
+import imgCat1 from "./assets/img/cat1.jpg";
 
 let catsInitial = [
   {
     name: "Кеша",
     consumption: 2,
+    image: imgCat1
   },
-  {name: "Рыжик"},
-  {name: "Пыжик"},
-  {name: "Мурка",consumption: 1},
+  { name: "Рыжик" },
+  { name: "Пыжик" },
+  {
+    name: "Мурка",
+    consumption: 1,
+    image: "https://w.forfun.com/fetch/5c/5c440772f54fc8f40bc80a130eb18565.jpeg"
+  },
 ];
 
 function App() {
@@ -22,24 +27,26 @@ function App() {
 
   const handleClick = (element) => {
     element.preventDefault();
-    setCats([
-      {name: "Новый кот без имени"},
-      ...cats,
-    ]);
+    setCats([{ name: "Новый кот без имени" }, ...cats]);
   };
   const handleChange = (e) => {
     let value = e.target.value;
-    let filter = cats.filter( (cat)=> cat.name.includes(value));
+    let filter = cats.filter((cat) => cat.name.includes(value));
     setFilteredCats(filter);
-  }
+  };
   return (
     <div className="container">
       <h1>Cats</h1>
 
-      <TextField onChange={handleChange} id="outlined-basic" label="Фильтр котов" variant="outlined" />
+      <TextField
+        onChange={handleChange}
+        id="outlined-basic"
+        label="Фильтр котов"
+        variant="outlined"
+      />
 
       {filteredCats.map((cat) => (
-        <Card name={cat.name} foodVolume={cat.consumption} />
+        <Card name={cat.name} foodVolume={cat.consumption} image={cat.image}/>
       ))}
 
       <button onClick={handleClick}>Добавить котов</button>
--- lesson1/app1/src/components/Card.js
+++ lesson1/app1/src/components/Card.js
@@ -1,20 +1,19 @@
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import imgDefaultCat from "../assets/img/default-cat.jpg";
 
 const Card = function (props) {
   const food = props.foodVolume ? props.foodVolume : "";
-  
+
   return (
     <div className="card">
       <img
-        src="https://media.moddb.com/images/groups/1/3/2392/cat.jpg"
-        alt=""
+        src={props.image ? props.image : imgDefaultCat}
+        alt={props.name}
         className="card__image"
       />
       <div className="card__description">
         <h2>{props.name}</h2>
-        {food ? (
-          <p>Котик кушает {food}кг</p>
-        ) : "" }
+        {food ? <p>Котик кушает {food}кг</p> : ""}
 
         <Button variant="contained">Погладить кота</Button>
       </div>
