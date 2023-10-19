f35f4dab69bccc209a55af736122b31decac5b04
Danil <danil@davletov.net>
2023-10-19 15:28:01 +0300

    fix add cat

--- lesson1/app1/src/App.js
+++ lesson1/app1/src/App.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
 import Card from "./components/Card";
-import Button, { Button2 } from "./components/Button";
+import { Button } from "@mui/material";
 import { TextField } from "@mui/material";
 
 import imgCat1 from "./assets/img/cat1.jpg";
+import AddIcon from '@mui/icons-material/Add';
 
 let catsInitial = [
   {
@@ -23,7 +24,7 @@ let catsInitial = [
 
 function App() {
   const [cats, setCats] = useState(catsInitial);
-  const [filteredCats, setFilteredCats] = useState(catsInitial);
+  const [filteredCats, setFilteredCats] = useState(cats);
 
   const handleClick = (element) => {
     element.preventDefault();
@@ -34,6 +35,11 @@ function App() {
     let filter = cats.filter((cat) => cat.name.includes(value));
     setFilteredCats(filter);
   };
+
+  useEffect(()=>{
+    console.log("Котик добавлен");
+    setFilteredCats(cats);
+  },[cats]);
   return (
     <div className="container">
       <h1>Cats</h1>
@@ -49,7 +55,7 @@ function App() {
         <Card name={cat.name} foodVolume={cat.consumption} image={cat.image}/>
       ))}
 
-      <button onClick={handleClick}>Добавить котов</button>
+      <Button variant="contained" color="success" size="large" startIcon={<AddIcon/>}  onClick={handleClick}>Добавить котов</Button>
     </div>
   );
 }
